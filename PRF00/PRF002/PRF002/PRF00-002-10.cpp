067c850753dccb6be5979b99df75d1387c3b7c30
Nguyen Viet Man <128305002+oooreiP@users.noreply.github.com>
2024-10-01 12:31:51 +0700

    Add files via upload

---
+++ PRF00/PRF002/PRF002/PRF00-002-10.cpp
@@ -0,0 +1,18 @@
+/*
+Thực hành toán tử quan hệ #2
+Cập nhật tháng 3 năm 2023
+
+Viết chương trình nhập vào 1 số nguyên lớn hơn 0 và kiểm tra xem số nguyên đó có phải là số chẵn hay không. Nếu là số chẵn thì in ra 1, ngược lại thì in ra 0.
+*/
+#include <iostream>
+
+int main()
+{
+    int a;
+
+    std::cin >> a;
+
+    // Viết code ở đây
+    
+    return 0;
+}
