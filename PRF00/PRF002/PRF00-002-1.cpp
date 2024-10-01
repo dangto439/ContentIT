3614dd84e3bd77e185cfe3be288d1d67c35005b5
Nguyen Viet Man <128305002+oooreiP@users.noreply.github.com>
2024-10-01 12:32:53 +0700

    Add files via upload

---
+++ PRF00/PRF002/PRF00-002-1.cpp
@@ -0,0 +1,14 @@
+
+/*Thực hành khai báo biến
+Cập nhật tháng 3 năm 2023
+
+Hãy khởi tạo 1 biến có kiểu dữ liệu là số nguyên, gán cho nó giá trị là 100 và in ra màn hình giá trị của biến đó.#include <iostream>
+*/
+#include <iostream>
+int main()
+{
+    // Viết code ở đây
+    int i = 100;
+    std::cout << i;
+    return 0;
+}
---
+++ PRF00/PRF002/PRF00-002-8.cpp
@@ -0,0 +1,20 @@
+/*
+Viết chương trình nhập vào lần lượt số ngày, số giờ, số phút và số giây. Hãy tính và in tổng số giây ra màn hình.
+Ví dụ:
+Đầu vào:
+1 2 3 4
+Đầu ra:
+93784
+*/
+#include <iostream>
+
+int main()
+{
+    int days, hours, minutes, seconds;
+    
+    std::cin >> days >> hours >> minutes >> seconds;
+
+    // Viết code ở đây
+    
+    return 0;
+}
